81ef6218ded21ded1ce48d4ef5c530ea35374284
J Albert <immortalsantiago@gmail.com>
2026-05-23 15:29:51 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['i.imgur.com', 'placehold.co', 'www.image2url.com'],
+  },
+}
+
+module.exports = nextConfig
